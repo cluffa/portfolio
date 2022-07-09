acc8295507179c154f2d623ac777dd738c6e8692
cluffa <alexcluff16@gmail.com>
2022-07-09 01:01:41 -0400

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayiwfdash() {
+    document.getElementById("iwfdash").innerHTML = "<div class=\"dash_wrapper\"><iframe src=\"https://cluffa.shinyapps.io/IWF-Data-Explorer/\" frameborder=\"0\" width=\"100%\"height=\"100%\"></iframe></div>";
+}
+
+function displaywldash() {
+    document.getElementById("wldash").innerHTML = "<div class=\"dash_wrapper\"><iframe src=\"https://cluffa.shinyapps.io/Weight-Loss-Trends/\" frameborder=\"0\" width=\"100%\"height=\"100%\"></iframe></div>";
+}
