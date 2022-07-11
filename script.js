598bdb0b3917ad639e8b700aa7b55b8ff7e41ede
cluffa <alexcluff16@gmail.com>
2022-07-10 20:01:51 -0400

    hide button

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function displayiwfdash() {
-    document.getElementById("iwfdash").innerHTML = "<div class=\"dash_wrapper\"><iframe src=\"https://cluffa.shinyapps.io/IWF-Data-Explorer/\" frameborder=\"0\" width=\"100%\"height=\"100%\"></iframe></div>";
+function display(id, url) {
+    // iwfdash https://cluffa.shinyapps.io/IWF-Data-Explorer/
+    // wldash https://cluffa.shinyapps.io/Weight-Loss-Trends/
+    document.getElementById(id).innerHTML = "<div class=\"dash_wrapper\"><iframe src=\"" +
+        url + "\" frameborder=\"0\" width=\"100%\"height=\"100%\"></iframe></div>";
 }
 
-function displaywldash() {
-    document.getElementById("wldash").innerHTML = "<div class=\"dash_wrapper\"><iframe src=\"https://cluffa.shinyapps.io/Weight-Loss-Trends/\" frameborder=\"0\" width=\"100%\"height=\"100%\"></iframe></div>";
+function hide(id) {
+    document.getElementById(id).innerHTML = "";
 }
